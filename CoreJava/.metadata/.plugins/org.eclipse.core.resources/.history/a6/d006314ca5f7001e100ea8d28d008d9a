7b4c71bf743e3f7ed55c5402e4e0a3a121296d7f
PavanKumar2739 <124656526+PavanKumar2739@users.noreply.github.com>
2024-04-11 08:40:55 +0530

    recent changes

---
+++ CoreJava/.metadata/.plugins/org.eclipse.core.resources/.history/a6/d006314ca5f7001e100ea8d28d008d9a
@@ -0,0 +1,8 @@
+package recursions;
+
+public class NumbersExample {
+	public static void main(String[] args) {
+		//print numbers write a function that takes and prints it
+	}
+
+}
--- CoreJava/DSA/src/recursions/NumbersExample.java
+++ CoreJava/DSA/src/recursions/NumbersExample.java
@@ -3,6 +3,34 @@ package recursions;
 public class NumbersExample {
 	public static void main(String[] args) {
 		//print numbers write a function that takes and prints it
+		//print first 5 numbers 
+		
+		print1(1);
+	}
+
+	private static void print1(int i) {
+		System.out.println(i);
+		print2(2);
+		
+	}
+	private static void print2(int i) {
+		System.out.println(i);
+		print3(3);
+		
+	}
+	private static void print3(int i) {
+		System.out.println(i);
+		print4(4);
+		
+	}
+	private static void print4(int i) {
+		System.out.println(i);
+		print5(5);
+		
+	}
+	private static void print5(int i) {
+		System.out.println(i);
+		
 	}
 
 }
